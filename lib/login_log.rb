50ebf4dc1182744ab7eaf575b21dc6a25a731cf3
Robert Sanders <robert@esquimaux.org>
2009-10-19 04:37:43 -0400

    make this friendlier to our console

--- lib/login_log.rb
+++ lib/login_log.rb
@@ -13,5 +13,25 @@ class LoginLog < ActiveRecord::Base
      } } 
  
   named_scope :failed, :conditions => ['failed = ?', true]
-  
+
+  # include Equid::CommonScopes
+
+  named_scope :for_user, lambda {|user|
+    return {} if user.blank?
+    idlike = case user
+      when User then user.id
+      else user.to_s
+    end
+    { :joins => ["left outer join users on #{self.table_name}.user_id = users.id"],
+      :conditions => ["#{self.table_name}.user_id = ? OR #{self.table_name}.login = ? OR users.login = ?",
+                      idlike, idlike, idlike] }
+  }
+
+  acts_as_scoped_search
+  searchable_on do
+    scope :text_search
+    fields :login, :created_at, :ip_address
+  end
+
+
 end
