0e88ec80d4113199391cd541b0bfc072efa3d000
Robert Sanders <robert@esquimaux.org>
2010-10-10 18:41:26 -0400

    smarter login_logger

--- lib/login_logger.rb
+++ lib/login_logger.rb
@@ -2,16 +2,39 @@
 module Brainnovate
   module LoginLogger
     
-    def log_login
-      unless current_user.blank?
-        LoginLog.create(:user_id => current_user.id,
-                        :login => current_user.login,
-                        :ip_address => request.remote_ip,
+    def log_login(options = {})
+      ip = respond_to?(:session_public_ip) ? session_public_ip : request.remote_ip
+      async = options.delete(:async)
+      user = options.delete(:user) || (options[:failed] ? nil : current_user)
+      options[:login_method] = options.delete(:method) || options.delete(:login_method)
+
+      options = {
+              :user_permtoken => cookies[:_zx_perm_uid],
+              :user_agent => request.user_agent,
+              :session_id => session[:session_id],
+              :domain => request.host
+      }.merge(options)
+
+      options[:login_method] = options[:login_method].to_s if options[:login_method]
+
+      unless user.blank?
+        log = LoginLog.new({:user_id => user.id,
+                        :login => user.login,
+                        :ip_address => ip,
                         :failed => false
-                        )
+                        }.merge(options))
       else
-        LoginLog.create(:login => params[:login],
-                        :ip_address => request.remote_ip)    
+        log = LoginLog.new({:login => params[:login],
+                            :failed => true,
+                            :ip_address => ip}.merge(options))
+      end
+
+      if async && false
+        run_later do
+          log.save
+        end
+      else
+        log.save
       end
     end
   end
